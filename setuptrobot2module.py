64ba70ab9de0d18856759c2561a482d86e95ae6b
thehomemadecode <thehomemadecode@gmail.com>
2024-02-24 23:33:15 +0300

    Add files via upload

---
+++ setuptrobot2module.py
@@ -0,0 +1,12 @@
+from distutils.core import setup, Extension
+
+def main():
+    setup(name="trobot2",
+          version="2.0.0",
+          description="Python interface for the trobot C library function",
+          author="thehomemadecode",
+          author_email="thehomemadecode@gmail.com",
+          ext_modules=[Extension("trobot2", ["trobot2module.c"])])
+
+if __name__ == "__main__":
+    main()
