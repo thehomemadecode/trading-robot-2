1240668192ee700ecafa2fcfcc1d5ed2b78f60ab
thehomemadecode <thehomemadecode@gmail.com>
2024-03-01 14:59:10 +0300

    Update makeCandlestickDB.py

--- makehistory/makeCandlestickDB.py
+++ makehistory/makeCandlestickDB.py
@@ -40,7 +40,7 @@ def main():
     graphtimeperiodlist = ["5m", "1h", "1d"]
     prefix = "tr2"
     dbfilename = "candlestick.db"
-    limit = 3
+    limit = 100
     
     # show infos
     file = open(filename, "r")
@@ -69,4 +69,3 @@ def main():
 # most probably main
 if __name__ == '__main__':
     main()
-    
