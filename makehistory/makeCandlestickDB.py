470e118a16bdb35acbb23b90adecf22c3aa2886f
thehomemadecode <thehomemadecode@gmail.com>
2024-03-04 23:55:24 +0300

    Update makeCandlestickDB.py

--- makehistory/makeCandlestickDB.py
+++ makehistory/makeCandlestickDB.py
@@ -37,7 +37,7 @@ def main():
     fx = fxtypes[1]
     status = statustypes[1]
     #graphtimeperiodlist = ["1s", "1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "6h", "8h", "12h", "1d", "3d", "1w", "1M"]
-    graphtimeperiodlist = ["5m", "1h", "1d"]
+    graphtimeperiodlist = ["5m"]
     prefix = "tr2"
     dbfilename = "candlestick.db"
     limit = 40
