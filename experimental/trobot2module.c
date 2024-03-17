ef1abe22dbe5e137ca33d066fcfec7edc1978b78
thehomemadecode <thehomemadecode@gmail.com>
2024-03-17 21:44:55 +0300

    working on module C 17Mar

--- experimental/trobot2module.c
+++ experimental/trobot2module.c
@@ -1,47 +1,109 @@
 #include <Python.h>
+#include <stdio.h>
+#include <string.h>
 
-static PyObject *check(PyObject *self, PyObject *args) {
-	PyObject* data;
-	if (!PyArg_ParseTuple(args, "i", &data)) {
-		return NULL;
-	}
-	data = subcheck(data);
-	return Py_BuildValue("i", data);
+/* TA functions section begin */
+double sma(double *data) {
+    double total = 0;
+    int len = 2;
+    for (int i=0; i<len; i++) {
+        total += data[i];
+    }
+    total = total / len;
+    return total;
 }
 
-int subcheck(int x) {
-	x++;
-  return x;
+double ema(double *data) {
+    double total = 0;
+    int len = 2;
+    for (int i=0; i<len; i++) {
+        total += data[i];
+    }
+    total = total / len;
+    return total;
 }
+/* TA functions section end */
 
-static PyObject *sma(PyObject *self, PyObject *args) {
-    PyObject* py_list;
-	PyObject* py_list2;
-	PyObject* item;
-	int ohlc = 0;
-	int period = 0;
-	double sonuc = 0;
-	double value = 0;
-    if (!PyArg_ParseTuple(args, "Oii", &py_list, &ohlc, &period)) {
+/* function map section begin */
+typedef double (*func_ptr_double)(double*);
+typedef struct {
+    const char *name;
+    func_ptr_double func;
+} FunctionMap;
+FunctionMap functions[] = {
+    {"sma", (func_ptr_double)sma}, // Yeniden dönüştürme yapılır
+    {"ema", (func_ptr_double)ema}, // Yeniden dönüştürme yapılır
+};
+func_ptr_double get_function(const char *name) {
+    for (size_t i = 0; i < sizeof(functions) / sizeof(functions[0]); ++i) {
+        if (strcmp(name, functions[i].name) == 0) {
+            return functions[i].func;
+        }
+    }
+    return NULL; // Eşleşme yoksa NULL döndür
+}
+/* function map section end */
+
+/* Reception function for incoming data: */
+static PyObject *receptionC(PyObject *self, PyObject *args) {
+    PyObject* data;
+    PyObject* cols;
+    PyObject* analysisrules;
+    if (!PyArg_ParseTuple(args, "OOO", &data, &cols, &analysisrules)) {
         return NULL;
     }
-	Py_ssize_t size = PyList_Size(py_list);
-	for (Py_ssize_t i = size-1; i > size-period-1; i--) {
-		py_list2 = PyList_GetItem(py_list, i);
-		item = PyList_GetItem(py_list2, ohlc);
-		value = PyFloat_AsDouble(item);
-		sonuc = sonuc + value;
-	}
-	sonuc = sonuc / period;
-	return Py_BuildValue("d", sonuc);
-}
 
+    Py_ssize_t num_rows = PyList_Size(data);
+    Py_ssize_t num_cols = 0;
+    double **data_c = malloc(num_rows * sizeof(double *));
+    for (Py_ssize_t i = 0; i < num_rows; ++i) {
+        PyObject *row = PyList_GetItem(data, i);
+        num_cols = PyList_Size(row);
+        data_c[i] = malloc(num_cols * sizeof(double));
+        for (Py_ssize_t j = 0; j < num_cols; ++j) {
+            PyObject *item = PyList_GetItem(row, j);
+            data_c[i][j] = PyFloat_AsDouble(item);
+        }
+    }
+
+    Py_ssize_t len_cols = PyList_Size(cols);
+    int cols_c[len_cols];
+    for (Py_ssize_t i = 0; i < len_cols; ++i) {
+        PyObject *item = PyList_GetItem(cols, i);
+        cols_c[i] = (int)PyLong_AsLong(item);
+    }
+
+    Py_ssize_t len = PyList_Size(analysisrules);
+    const char *function_names[len];
+    for (Py_ssize_t i = 0; i < len; ++i) {
+        PyObject *item = PyList_GetItem(analysisrules, i);
+        const char *name = PyUnicode_AsUTF8(item);
+        function_names[i] = name;
+    }
+    
+    for (size_t i = 0; i < sizeof(function_names) / sizeof(function_names[0]); ++i) {
+        const char *name = function_names[i];
+        func_ptr_double func = get_function(name);
+        for (Py_ssize_t j=0; j<num_rows; j++) {
+            printf("%s = %lf\n", name, func(data_c[j]));
+        }
+    }
+
+
+
+
+
+    double signal = 0;
+    double signal2 = 0;
+    return Py_BuildValue("dd", signal, signal2);
+}
+/* Reception function end */
+
+/* for Python.h to the end of code */
 static PyMethodDef trobot2Methods[] = {
-    {"check", check, METH_VARARGS, "check"},
-    {"sma", sma, METH_VARARGS, "Simple moving average"},
+    {"receptionP", receptionC, METH_VARARGS, "Reception function for incoming data."},
     {NULL, NULL, 0, NULL}
 };
-
 static struct PyModuleDef trobot2 = {
     PyModuleDef_HEAD_INIT,
     "trobot2",
@@ -49,7 +111,7 @@ static struct PyModuleDef trobot2 = {
     -1,
     trobot2Methods
 };
-
 PyMODINIT_FUNC PyInit_trobot2(void) {
     return PyModule_Create(&trobot2);
 }
+/* end of code */
--- experimental/work.py
+++ experimental/work.py
@@ -1,9 +1,10 @@
 import random
+import trobot2 as tr
 
 alpha = 1
 beta = 1.5
 wbvnum = random.weibullvariate(alpha, beta)
-print(round(wbvnum,4))
+#print(round(wbvnum,4))
 
 datasend = []
 for d in range(0,3):
@@ -12,18 +13,34 @@ for d in range(0,3):
         wbvnum = random.weibullvariate(alpha, beta)
         row.append(round(wbvnum,4))
     datasend.append(row)
-print(datasend)
+#print(datasend)
 
 row = []
 for i in range(0,6):
     wbvnum = random.weibullvariate(alpha, beta)
     row.append(round(wbvnum,4))
 datasend[0] = row
-print(datasend)
+#print(datasend)
 
 row = []
 for i in range(0,6):
     wbvnum = random.weibullvariate(alpha, beta)
     row.append(round(wbvnum,4))
 datasend = [row] + datasend
-print(datasend)
+#print(datasend)
+
+cols = [0,1,2,3,4,5]
+analysisrules = ["sma","ema"]
+
+
+for i in range(0,len(datasend)):
+    res = (datasend[i][0]+datasend[i][1])/2
+    res = round(res,4)
+    print(res)
+#print(datasend)
+#print(cols)
+#print(analysisrules)
+res = tr.receptionP(datasend,cols,analysisrules)
+#print(res[0])
+#print(res[1])
+
