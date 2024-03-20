c0292d37127f291e8f7b8c5bda14cc17aaf3e386
thehomemadecode <thehomemadecode@gmail.com>
2024-03-20 11:25:04 +0300

    Mar20

--- experimental/work.py
+++ experimental/work.py
@@ -1,6 +1,38 @@
 import random
 import trobot2 as tr
 
+# read config.ini
+def init_config(filename):
+    config = {}
+    current_section = None
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line.startswith('[') and line.endswith(']'):
+                current_section = line[1:-1]
+                config[current_section] = {}
+            elif '=' in line and current_section and not line.startswith('#'):
+                key, value = line.split('=')
+                config[current_section][key.strip()] = value.strip()
+    file.close()
+    return config
+
+# read rules from config.ini
+def init_rules(config_rules):
+    allrules = []
+    for item in config_rules:
+        items = []
+        allrules.append(items)
+        items.append(item)
+        items.append(config_rules[f'{item}'])
+    return allrules
+
+filename = "config.ini"
+config = init_config(filename)
+allrules = init_rules(config['rules'])
+print(allrules)
+
+
 alpha = 1
 beta = 1.5
 wbvnum = random.weibullvariate(alpha, beta)
@@ -31,11 +63,7 @@ print(datasend)
 
 cols = [0,1,2,3,4,5]
 
-#analysisrule = "sma(10)>ema(9)<hma(30)"
-#analysisrule = "hma(20)>ema(7)"
-
-#analysisrule = "sma(10)>high>close"
-analysisrule = "hma(5)>open"
+analysisrule = allrules[1][1]
 
 
 
