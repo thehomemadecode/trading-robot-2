94eb17fedbeb037692a0cfa34890c1ac4f964566
thehomemadecode <thehomemadecode@gmail.com>
2024-05-07 15:54:56 +0300

    Release v2.0.0b

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 def main():
     setup(name="trobot2",
-          version="2.0.0b",
+          version="2.0.0",
           description="Python interface for the trobot C library function",
           author="thehomemadecode",
           author_email="thehomemadecode@gmail.com",
